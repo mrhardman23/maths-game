2145c05bcee83960f930d87f928515549999a7ff
Scott Hardman <scott.hardman23@gmail.com>
2022-05-05 13:30:50 -0500

    Final update

--- scripts/math-logic.js
+++ scripts/math-logic.js
@@ -27,7 +27,10 @@ startButton.onclick = function() {
     score = 0;
     timeLeft = 60;
 
+    document.getElementById("question").focus();
+
     timeLeftBox.style.visibility = "visible";
+    timeLeftBox.ariaHidden="false";
 
     scoreDisplay.innerHTML = score;
     timeDisplay.innerHTML = timeLeft;
@@ -58,6 +61,7 @@ for(i = 0; i < document.getElementsByClassName("answers").length; i++){
 
         wrongBox.style.display="none";
         correctBox.style.display="block";
+        correctBox.focus();
         setTimeout(function (){
           correctBox.style.display="none";
         }, 1000);
@@ -67,6 +71,7 @@ for(i = 0; i < document.getElementsByClassName("answers").length; i++){
       else {
         correctBox.style.display="none";
         wrongBox.style.display="block";
+        wrongBox.focus();
         setTimeout(function (){
           wrongBox.style.display="none";
         }, 1000);
@@ -84,6 +89,7 @@ function startCountdown() {
       stopCountdown();
       gameOverBox.style.visibility = "visible";
       gameOverScore.innerHTML = score;
+      gameOverBox.focus();
     }
   }, 1000);
 }
@@ -121,4 +127,6 @@ function generateQA (){
       answers.push(wrongAnswer);
     }
   }
+
+  document.getElementById("question").focus();
 }
